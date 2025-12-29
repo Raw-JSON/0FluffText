f5a50a8052d4351f77dd9f9c4bbae4795795ff7f
0Fluff <jayancobky567@gmail.com>
2025-12-29 04:29:41 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// 0FluffText Service Worker - v1.4.2
-const CACHE_NAME = '0FluffText-v1-4-2-cache';
+// 0FluffText Service Worker - v1.4.3
+const CACHE_NAME = '0FluffText-v1-4-3-cache';
 
 const urlsToCache = [
     './',
